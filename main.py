d949476753562f0775e6be0f2c07f75efbe9ce39
Duong Van Hieu <hieudv@ait.com>
2020-09-24 16:39:31 +0700

    Crawl data ten gia anh laptop dien thoai

--- main.py
+++ main.py
@@ -11,54 +11,55 @@ import txt
 #Phan Tich lay data cua san pham
 def Sort_data(r):
     soup = BeautifulSoup(r.content, "html.parser")
-    title = soup.find('div', class_='rowtop').find('h1').text
-    area_price = soup.find('div', class_='area_price').find('strong').text
-    hisprice = soup.find('span', class_='hisprice').text
-    img = soup.find('div', class_='icon-position').find('img').attrs['src']
-    cpu = soup.find('li', class_='p224582 g92_94_93').find('div').text
-    ram = soup.find('li', class_='p224582 g146_149_155').find('div').text
-    disk = soup.find('li', class_='p224582 g184').find('div').text
-    man_hinh = soup.find('li', class_='p224582 g187_189').find('div').text
-    card_mh = soup.find('li', class_='p224582 g193_191').find('div').text
-    cong_kn = soup.find('li', class_='p224582 g200').find('div').text
-    os = soup.find('li', class_='p224582 g8599').find('div').text
-    thiet_ke = soup.find('li', class_='p224582 g7903_11082').find('div').text
-    kich_thuoc = soup.find('li', class_='p224582 g254_255').find('div').text
-    time_of_issue = soup.find('li', class_='p224582 g22711').find('div').text
+    title = soup.find('div', class_='rowtop')
+    area_price = soup.find('div', class_='area_price')
+    #hisprice = soup.find('span', class_='hisprice')
+    img = soup.find('div', class_='icon-position')
+    try:
+        title = title.find('h1').text
+        area_price=area_price.find('strong').text
+        img = img.find('img').attrs['src']
+        #hisprice=hisprice.text
+    except:
+        print("San Pham Khong Dong form")
+        title=''
+        area_price=''
+        img=''
+        #hisprice=''
+    #area_price = soup.find('div', class_='area_price').find('strong').text
+    #hisprice = soup.find('span', class_='hisprice').text
+    #img = soup.find('div', class_='icon-position').find('img').attrs['src']
+    # cpu = soup.find('li', class_='p224582 g92_94_93').find('div').text
+    # ram = soup.find('li', class_='p224582 g146_149_155').find('div').text
+    # disk = soup.find('li', class_='p224582 g184').find('div').text
+    # man_hinh = soup.find('li', class_='p224582 g187_189').find('div').text
+    # card_mh = soup.find('li', class_='p224582 g193_191').find('div').text
+    # cong_kn = soup.find('li', class_='p224582 g200').find('div').text
+    # os = soup.find('li', class_='p224582 g8599').find('div').text
+    # thiet_ke = soup.find('li', class_='p224582 g7903_11082').find('div').text
+    # kich_thuoc = soup.find('li', class_='p224582 g254_255').find('div').text
+    # time_of_issue = soup.find('li', class_='p224582 g22711').find('div').text
     Thong_so_kt = []
     Thong_Tin_SP = []
     Thong_so_kt.append({
-        "CPU": cpu,
-        "RAM": ram,
-        "DISK": disk,
-        "Man Hinh": man_hinh,
-        "Card Man Hinh": card_mh,
-        "Cong ket Noi": cong_kn,
-        "HDH": os,
-        "Thiet Ke": thiet_ke,
-        "kich Thuoc": kich_thuoc,
-        "Thoi Gian Ra Mat": time_of_issue
+        # "CPU": cpu,
+        # "RAM": ram,
+        # "DISK": disk,
+        # "Man Hinh": man_hinh,
+        # "Card Man Hinh": card_mh,
+        # "Cong ket Noi": cong_kn,
+        # "HDH": os,
+        # "Thiet Ke": thiet_ke,
+        # "kich Thuoc": kich_thuoc,
+        # "Thoi Gian Ra Mat": time_of_issue
     })
     Thong_Tin_SP.append({
         "Ten San Pham": title,
         "Gia Sale": area_price,
-        "Gia Goc": hisprice,
-        "Thong So KT": Thong_so_kt,
+        #"Gia Goc": hisprice,
+        #"Thong So KT": Thong_so_kt,
         "Link Anh": img
     })
-    # print(title)
-    # print(area_price)
-    # print(hisprice)
-    # print(cpu)
-    # print(ram)
-    # print(disk)
-    # print(man_hinh)
-    # print(card_mh)
-    # print(cong_kn)
-    # print(os)
-    # print(thiet_ke)
-    # print(kich_thuoc)
-    # print(time_of_issue)
     print(Thong_Tin_SP)
 
 
@@ -79,6 +80,7 @@ def getlinksp(base_url,url):#https://www.thegioididong.com,https://www.thegioidi
     links = [link.find('a').attrs['href'] for link in titles]
     # print(links)
     # lay link tung san pham luu vao file urls.txt
+    count=0
     with open('urls.txt', 'w') as outfile:
         for link in links:
             data = base_url + link
@@ -86,19 +88,23 @@ def getlinksp(base_url,url):#https://www.thegioididong.com,https://www.thegioidi
             r = requests.get(data)
             #print(r)
             Sort_data(r)
+            count= count + 1
             if data:
                 # txt.dump(data, outfile)
                 outfile.write(data)
                 outfile.write("\n")
                 # sleep(5)
-                print('-------------------------------------------------------------------')
 
+                print(count)
 
-#getlinksp('https://www.thegioididong.com','https://www.thegioididong.com/laptop')
-
-r = requests.get("https://www.thegioididong.com/laptop/hp-348-g7-i3-9pg83pa")
-# print(r)
-Sort_data(r)
+#get data laptop
+#laptop=getlinksp('https://www.thegioididong.com','https://www.thegioididong.com/laptop')
+#Get Data dien thoai di dong
+dt=getlinksp('https://www.thegioididong.com','https://www.thegioididong.com/dtdd#i:3')#1=28 ,2,3
+#
+# r = requests.get("https://www.thegioididong.com/laptop/hp-348-g7-i3-1a0z1pa")
+# # print(r)
+# Sort_data(r)
 
 
 
